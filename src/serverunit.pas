34ab5b8006b779f22b0398eb6832ce93313c20db
Jorge Luis <jorgequiguango@outlook.com>
2025-03-29 09:42:04 -0500

    [ADD] BeforeRequestHandler for CORS preflight request

--- src/serverunit.pas
+++ src/serverunit.pas
@@ -3,6 +3,7 @@
   Jorge Luis
   jorgeluis@resolvedor.dev
   https://resolvedor.dev
+  2025
 *)
 
 unit ServerUnit;
@@ -26,6 +27,8 @@ type
     procedure Ping(Request: TRequest; Response: TResponse);
     procedure CatchAll(Request: TRequest; Response: TResponse);
     procedure TextArray(Request: TRequest; Response: TResponse);
+    procedure BeforeRequestHandler(Sender: TObject; Request: TRequest;
+      Response: TResponse);
     function BearerTokenValidate(Request: TRequest; Response: TResponse): boolean;
   end;
 
@@ -40,6 +43,21 @@ const
 procedure TServer.Start;
 begin
   Application.Port := 9090;
+  (*
+  A CORS preflight request is a CORS request that checks to see if the CORS protocol
+  is understood and a server is aware using specific methods and headers.
+
+  It is an OPTIONS request, using two or three HTTP request headers:
+  Access-Control-Request-Method, Origin, and optionally Access-Control-Request-Headers.
+
+  For CORS error:
+  has been blocked by CORS policy:
+  Response to preflight request doesn't pass access control check:
+  It does not have HTTP ok status
+
+  HTTPRouter.BeforeRequest := @BeforeRequestHandler;
+  *)
+  HTTPRouter.BeforeRequest := @BeforeRequestHandler;
   HTTPRouter.RegisterRoute('/ping', rmGet, @Ping);
   HTTPRouter.RegisterRoute('/catchall', rmAll, @CatchAll, True);
   HTTPRouter.RegisterRoute('/print', rmPost, @TextArray);
@@ -54,8 +72,8 @@ procedure TServer.Ping(Request: TRequest; Response: TResponse);
 begin
 
   try
-    //PrinterPos.Ping;
-    Response.Content := '{"message":"pong"}';
+    PrinterPos.Ping;
+    Response.Content := '{"message":"Pong"}';
     Response.Code := 200;
   except
     on E: Exception do
@@ -66,7 +84,8 @@ begin
     end;
   end;
   Response.SetCustomHeader('Access-Control-Allow-Origin', '*');
-  Response.SetCustomHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  Response.SetCustomHeader('Access-Control-Allow-Headers',
+    'Content-Type, Authorization');
   Response.SetCustomHeader('Access-Control-Allow-Methods', 'POST, GET');
   Response.SetCustomHeader('Connection', 'Keep-Alive');
   Response.ContentType := 'application/json';
@@ -80,7 +99,8 @@ begin
   Response.Content := '{"message":"This endpoint is not available"}';
   Response.Code := 404;
   Response.SetCustomHeader('Access-Control-Allow-Origin', '*');
-  Response.SetCustomHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  Response.SetCustomHeader('Access-Control-Allow-Headers',
+    'Content-Type, Authorization');
   Response.SetCustomHeader('Access-Control-Allow-Methods', 'POST, GET');
   Response.SetCustomHeader('Connection', 'Keep-Alive');
   Response.ContentType := 'application/json';
@@ -101,7 +121,7 @@ begin
   //  Exit;
   try
     jObject := GetJSON(Request.Content) as TJSONObject;
-    //PrinterPos.ArrayWriter(jObject);
+    PrinterPos.ArrayWriter(jObject);
 
     Response.Content := '{"message":"Printed"}';
     Response.Code := 200;
@@ -114,7 +134,8 @@ begin
     end;
   end;
   Response.SetCustomHeader('Access-Control-Allow-Origin', '*');
-  Response.SetCustomHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  Response.SetCustomHeader('Access-Control-Allow-Headers',
+    'Content-Type, Authorization, X-Requested-With');
   Response.SetCustomHeader('Access-Control-Allow-Methods', 'POST, GET');
   Response.SetCustomHeader('Connection', 'Keep-Alive');
   Response.ContentType := 'application/json';
@@ -122,6 +143,23 @@ begin
   Response.SendContent;
   WriteLn('[INFO] Request POST to /print/v1/text/array and response status: ' +
     IntToStr(Response.Code));
+end;
+
+procedure TServer.BeforeRequestHandler(Sender: TObject; Request: TRequest;
+  Response: TResponse);
+begin
+  Response.SetCustomHeader('Access-Control-Allow-Origin', '*');
+  Response.SetCustomHeader('Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS');
+  Response.SetCustomHeader('Access-Control-Allow-Headers',
+    'Content-Type, Accept, Accept-Language, Accept-Encoding');
+
+  if Request.Method = 'OPTIONS' then
+  begin
+    Response.Code := 200;
+    Response.SendResponse;
+    Exit;
+  end;
 end;
 
 function TServer.BearerTokenValidate(Request: TRequest; Response: TResponse): boolean;
